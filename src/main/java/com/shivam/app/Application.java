cb218c1f2ffbb496c005e6b0c66538097a7b70f1
shivamsharma1998 <sharmashivam986@gmail.com>
2019-08-05 13:31:36 -0400

    develop config

---
+++ src/main/java/com/shivam/app/Application.java
@@ -0,0 +1,11 @@
+package com.shivam.app;
+
+public class Application {
+
+    public static void main(String[] args){
+
+        if (args.length<1){
+            System.out.println("Application need config file to begin data generation");
+        }
+    }
+}
---
+++ src/main/java/com/shivam/config/Constants.java
@@ -0,0 +1,8 @@
+package com.shivam.config;
+
+public class Constants {
+    private static  String NUM_RECORDS;
+    private static String OUTPUT_FORMAT;
+    private static String OUTPUT_COLUMN;
+    private static String OUTPUT_COLUMN_DATATYPE;
+}
