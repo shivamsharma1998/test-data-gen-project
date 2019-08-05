60a3a8d7a9c4eaa4110473f3d90bdfe604eb00ff
shivamsharma1998 <sharmashivam986@gmail.com>
2019-08-05 11:33:08 -0400

    my initial commit

---
+++ src/main/java/com/shivam/App.java
@@ -0,0 +1,13 @@
+package com.shivam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
